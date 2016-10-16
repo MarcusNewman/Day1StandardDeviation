f67d87e0f4a7890d32468e02ae64c261f41c45d5
Marcus Newman <marcusgeorgenewman@gmail.com>
2016-10-15 17:42:31 -0700

    Completed

---
+++ Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        Console.ReadLine();
+        var X = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+
+        Console.WriteLine(StandardDeviation(X));
+    }
+    public static string StandardDeviation(int[] values)
+    {
+        var mean = Mean(values);
+        var valueMeanSquared = 0m;
+        foreach (var value in values)
+        {
+            var valueMean = value - mean;
+            valueMeanSquared += (valueMean * valueMean);
+        }
+        var variance = valueMeanSquared / values.Length;
+        return string.Format("{0:0.0}", Math.Sqrt((double)variance));
+    }
+
+    public static decimal Mean(int[] values)
+    {
+        var mean = 0m;
+        foreach (var element in values) mean += element;
+        return mean / values.Length;
+    }
+}
+
---
+++ Tests/SolutionUnitTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReflectionUnitTesting;
+using System.Reflection;
+
+[TestClass]
+public class SolutionUnitTests
+{
+    [TestMethod]
+    public void AssemblyShouldExist()
+    {
+        GetAssembly();
+    }
+
+    static Assembly GetAssembly()
+    {
+        return ReflectionAssert.AssemblyExists("Day1StandardDeviation.exe");
+    }
+
+    [TestMethod]
+    public void ClassShouldExist()
+    {
+        GetClass();
+    }
+
+    static Type GetClass()
+    {
+        return GetAssembly().TypeExists("Solution");
+    }
+
+    [TestMethod]
+    public void MethodShouldExist()
+    {
+        GetMethod();
+    }
+
+    static MethodInfo GetMethod()
+    {
+        return GetClass().MethodExists(
+            methodName: "StandardDeviation",
+            shouldBeStatic: true,
+            shouldReturnType: typeof(string),
+            parameterTypesAndNames: new System.Collections.Generic.List<Tuple<Type, string>> {
+                Tuple.Create(typeof(int[]), "values") });
+    }
+
+    [TestMethod]
+    public void StandardDeviationOf1ShouldBe0()
+    {
+        var parameters = new object[] { new int[] { 1 } };
+        var expectedResult = "0.0";
+        GetMethod().TestMethod(null, parameters, expectedResult);
+
+    }
+
+    //5
+    //10 40 30 50 20
+    //14.1
+    [TestMethod]
+    public void StandardDeviationOf1040305020ShouldBe141()
+    {
+        var parameters = new object[] { new int[] { 10,40,30,50,20 } };
+        var expectedResult = "14.1";
+        GetMethod().TestMethod(null, parameters, expectedResult);
+
+    }
+}
+
